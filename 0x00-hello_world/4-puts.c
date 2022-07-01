d71ac00f92884079d68221de07d4ec67818ca586
kitts makokha <kittsmakokha@gmail.com>
2022-07-01 00:41:48 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-/*a program that uses put to display a string
- *Returns 0 when program runs correctly
- */
-#include<stdio.h>
-int main(void)	
+#include <stdio.h>
+
+/**
+   * main - prints the phrase "Programming is like building a multilingual puzzle
+    * Return: 0 if exited properly, non-zero otherwise
+     */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-} 
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
