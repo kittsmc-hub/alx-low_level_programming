5a2ffad153330944698ce0f0878bfc752892b0f9
kitts makokha <kittsmakokha@gmail.com>
2022-07-01 01:36:34 +0000

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-/*a program that uses puts function */
 #include <stdio.h>
 
 /**
- * main prints phrase "Programming is like building a multilingual puzzle"
+ * main function prints phrase "Programming is like building a multilingual puzzle"
  * Return: 0 if exited properly
  */
 int main(void)
 {	
-    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
