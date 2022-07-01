043dc9b708367a9c5719dfa2904d5ecfd2756d4f
kitts makokha <kittsmakokha@gmail.com>
2022-07-01 20:42:45 +0000

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* main-function assigns a random number to a variable everytime its executed */
 int main(void)
 
 {
