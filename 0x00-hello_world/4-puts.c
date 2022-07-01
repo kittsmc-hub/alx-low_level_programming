81bcb46437c0788d0dad3c9c1570525606ae1bc6
kitts makokha <kittsmakokha@gmail.com>
2022-07-01 01:42:03 +0000

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main function prints phrase "Programming is like building a multilingual puzzle"
+ * main- prints phrase "Programming is like building a multilingual puzzle"
  * Return: 0 if exited properly
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
