ae43540a6598454b809ad4623548d211b841a86c
kitts <kittsmakokha@gmail.com>
2022-08-23 01:02:53 +0000

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,20 +20,20 @@ size_t _strlen(char *str)
 }
 
 /**
-  * append_text_to_file - appends a text at the end of a file.
-  * @filename: name of the file
-  * @text_content: NULL terminated string to add at the end of the file
+  * create_file - creates a file.
+  * @filename: name of the file to create
+  * @text_content: NULL terminated string to write to the file
   *
-  * Return: 1 on success and -1 on failure
+  * Return: 1 on success, -1 on failure
   */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
