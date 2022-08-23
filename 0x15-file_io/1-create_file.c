cbd5336f5b96c427301f6d012c4a47ecb731ba21
kitts <kittsmakokha@gmail.com>
2022-08-23 01:17:09 +0000

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,34 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 /**
-  * _strlen - finds the length of a string
-  * @str: pointer to the string
-  *
-  * Return: length of the string
-  */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-				;
-	return (i);
-}
-
-/**
-  * create_file - creates a file.
+  * create_file - creates a file and fills it with text
   * @filename: name of the file to create
-  * @text_content: NULL terminated string to write to the file
+  * @text_content: text to write in the file
   *
   * Return: 1 on success, -1 on failure
   */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int fd, x, y = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
+
+	if (text_content)
+	{
+		while (text_content[y])
+			y++;
+		x = write(fd, text_content, y);
+		if (x != y)
+			return (-1);
+	}
+
 	close(fd);
-	if (len == -1)
-		return (-1);
+
 	return (1);
 }
