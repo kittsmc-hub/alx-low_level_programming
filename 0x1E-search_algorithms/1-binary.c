563dec229e9b04f6619b8891da93cbb45b66cde8
kitts <kittsmakokha@gmail.com>
2023-02-02 21:25:35 +0000

    fixing errors

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,24 +1,21 @@
 #include "search_algos.h"
 
-int recurse_helper(int *array, size_t left,
-size_t right, int value, ssize_t *match);
+int recurse_helper(int *array, size_t left, size_t right, int value);
 
 /**
- * advanced_binary - search for value in array of sorted ints
+ * binary_search - search for value in array of sorted ints
  * @array: array to search
  * @size: size of array
  * @value: value to search
  *
  * Return: index of found value; or -1 if not found
  */
-int advanced_binary(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	ssize_t match = -1;
-
 	if (array == NULL)
 		return (-1);
 
-	return (recurse_helper(array, 0, size - 1, value, &match));
+	return (recurse_helper(array, 0, size - 1, value));
 }
 
 /**
@@ -27,17 +24,15 @@ int advanced_binary(int *array, size_t size, int value)
  * @left: leftmost index
  * @right: rightmost index
  * @value: value to search
- * @match: pointer to index of most recent match
  *
  * Return: index of found value; or -1 if not found
  */
-int recurse_helper(int *array, size_t left,
-size_t right, int value, ssize_t *match)
+int recurse_helper(int *array, size_t left, size_t right, int value)
 {
 	size_t i = left, mid;
 
 	if (left > right)
-		return (*match);
+		return (-1);
 
 	/* print search progress */
 	printf("Searching in array: %d", array[i++]);
@@ -50,17 +45,14 @@ size_t right, int value, ssize_t *match)
 
 	/* check if mid is value */
 	if (array[mid] == value)
+		return (mid);
+	else if (array[mid] > value)
 	{
-		*match = mid;
-		if (right - left > 1)
-			mid++;
+		if (mid != 0)
+			return (recurse_helper(array, left, mid - 1, value));
+		else
+			return (-1);
 	}
-	else if (array[mid] < value) /* search right */
-		return (recurse_helper(array, mid + 1, right, value, match));
-
-	if (mid != 0)
-		return (recurse_helper(array, left, mid - 1, value, match));
 	else
-		return (*match);
+		return (recurse_helper(array, mid + 1, right, value));
 }
-
